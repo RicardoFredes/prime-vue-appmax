0463f5ad8555b13941d178a762d167149d5c5b13
Fredes <ricardo.silveira@appmax.com.br>
2024-07-04 16:46:52 -0300

    feat(bank-accounts): add mobile data view

--- src/modules/withdraw/bank-accounts/BankAccountsView.vue
+++ src/modules/withdraw/bank-accounts/BankAccountsView.vue
@@ -4,6 +4,11 @@ import Button from 'primevue/button'
 import DataTable from 'primevue/datatable'
 import Column from 'primevue/column'
 import Paginator from 'primevue/paginator'
+import DataView from 'primevue/dataview'
+
+import { useMediaQuery } from '@vueuse/core'
+
+const showDataInTable = useMediaQuery('(min-width: 768px)')
 
 const bankAccounts = [
   {
@@ -34,7 +39,7 @@ const bankAccounts = [
       <Button size="small"><AmIcon name="add" />Cadastrar nova conta</Button>
     </header>
     <div class="space-y-4 bg-white rounded-lg shadow-sm lg:shadow-none pt-2">
-      <DataTable :value="bankAccounts">
+      <DataTable v-if="showDataInTable" :value="bankAccounts">
         <Column field="pixKey" header="Chave Pix"></Column>
         <Column field="pixType" header="Tipo"></Column>
         <Column field="bank" header="Banco"></Column>
@@ -48,6 +53,36 @@ const bankAccounts = [
           </template>
         </Column>
       </DataTable>
+      <DataView v-else :value="bankAccounts">
+        <template #list="slotProps">
+          <div
+            v-for="item in slotProps.items"
+            :key="item.id"
+            class="flex items-start justify-between p-6 border-b border-gray-200"
+          >
+            <ul class="space-y-1 text-sm">
+              <li>
+                <strong>Chave Pix:</strong> <span>{{ item.pixKey }}</span>
+              </li>
+              <li>
+                <strong>Tipo:</strong> <span>{{ item.pixType }}</span>
+              </li>
+              <li>
+                <strong>Banco:</strong> <span>{{ item.bank }}</span>
+              </li>
+              <li>
+                <strong>Empresa:</strong> <span>{{ item.companyName }}</span>
+              </li>
+              <li>
+                <strong>CNPJ/CPF:</strong> <span>{{ item.document }}</span>
+              </li>
+            </ul>
+            <Button text rounded title="Editar" style="padding: 0.75rem">
+              <AmIcon name="edit" />
+            </Button>
+          </div>
+        </template>
+      </DataView>
       <Paginator :rows="2" :totalRecords="1" />
     </div>
   </section>
