d91b6569fbc4802b8095792c7e1e73f0b8c226c5
Fredes <ricardo.silveira@appmax.com.br>
2024-07-29 11:40:55 -0300

    refactor(base): simplifica o uso do v3

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,9 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import routes from './routes'
 
-const basename = 'v3'
-const base = import.meta.env.BASE_URL + basename + '/'
-
+const base = 'v3'
 const history = createWebHistory(base)
 
 const router = createRouter({ history, routes, scrollBehavior: () => ({ left: 0, top: 0 }) })
